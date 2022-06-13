def832614a103fa600501746275366cea2c859fa
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-06-13 16:16:12 -0500

    mvp complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR DISPLAY";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_RECALL = "MEMORY_RECALL";
+export const MEMORY_CLEAR = "MEMORY_CLEAR";
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = operation => {
+    return {type: CHANGE_OPERATION, payload: operation};
+}
+
+export const clearDisplay = () => {
+    return {type: CLEAR_DISPLAY}
+}
+
+export const memoryAdd = () => {
+    return {type: MEMORY_ADD}
+}
+
+export const memoryRecall = () => {
+    return {type: MEMORY_RECALL};
+}
+
+export const memoryClear = () => {
+    return {type: MEMORY_CLEAR};
 }
